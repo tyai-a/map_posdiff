9b7162da903bad7466c813d4b917180254da6b59
tyai-a <achi@amr.co.jp>
2021-12-30 02:07:33 +0900

    基本機能を2つ実装
    -  サイドバーを2カラムにし、左に入力、右に入力データを表示するように
    -  inputを追加し、ユーザーが位置情報を追加できるように

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
-import { Wrap, WrapItem } from '@chakra-ui/react';
-import { Grid, GridItem } from '@chakra-ui/react';
-import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
+import { React, useEffect, useState, useReducer } from 'react';
+import { Formik, Form, Field } from 'formik';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  Button,
+  FormControl,
+  Text,
+  FormErrorMessage,
+  Input,
+  Wrap,
+  WrapItem,
+  Grid,
+  GridItem,
+  Stack,
+} from '@chakra-ui/react';
+import { FaPlus } from 'react-icons/fa';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import './styles.css';
 import 'leaflet/dist/leaflet.css';
 
 const R = Math.PI / 180;
-const data = [
-  {
-    name: '位置情報1',
-    lat: 35.666863,
-    lng: 139.84954,
-  },
-  {
-    name: '位置情報2',
-    lat: 35.663729,
-    lng: 139.744047,
-  },
-  {
-    name: '位置情報3',
-    lat: 43.064313,
-    lng: 141.347255,
-  },
-  {
-    name: '位置情報4',
-    lat: 35.663729,
-    lng: 139.744047,
-  },
-];
 
 /* 2点間の距離を計算し、kmで返却 */
 const distance = (lat1, lng1, lat2, lng2) => {
@@ -60,7 +55,7 @@ const checkArrayDistance = array => {
     const loopArrayDistance = getArrayNotNowLoopIndex.map((_x, _i) => {
       if (!_x) return undefined;
       return {
-        diff: `${x.name} > ${_x.name}: `,
+        diff: `${_x.name}との距離`,
         distance: distance(x.lat, x.lng, _x.lat, _x.lng),
       };
     });
@@ -83,16 +78,170 @@ const checkArrayDistance = array => {
   return processedArray;
 };
 
+const initialLocationList = [
+  {
+    name: '位置情報1',
+    lat: 35.666863,
+    lng: 139.84954,
+  },
+  {
+    name: '位置情報2',
+    lat: 35.663729,
+    lng: 139.744047,
+  },
+  {
+    name: '位置情報3',
+    lat: 43.064313,
+    lng: 141.347255,
+  },
+  {
+    name: '位置情報4',
+    lat: 35.663729,
+    lng: 139.744047,
+  },
+];
+
+const reducerLocation = (state, action) => {
+  switch (action.type) {
+    case 'addLocationItem':
+      return [...state, action.value];
+    case 'reset':
+      return initialLocationList;
+    default:
+      return state;
+  }
+};
+
+const validateLatitude = value => {
+  let error;
+  const isLatitude = isFinite(value) && Math.abs(value) <= 90;
+  if (!value) error = '入力必須です。';
+  if (!isLatitude) error = '形式が異なります。';
+  return error;
+};
+const validateLongitude = value => {
+  let error;
+  const isLongitude = isFinite(value) && Math.abs(value) <= 180;
+  if (!value) error = '入力必須です。';
+  if (!isLongitude) error = '形式が異なります。';
+  return error;
+};
+
 export default function App() {
+  // const [locationList, dispatchLocaion] = useReducer(reducerLocation, initialLocationList)
+  // const { locationList } = useLocationControl();
+  const [locationList, locationListDispatch] = useReducer(
+    reducerLocation,
+    initialLocationList
+  );
+
   return (
     <div className="App">
       <Grid
-        h="200px"
-        templateRows="repeat(2, 1fr)"
-        templateColumns="repeat(5, 1fr)"
+        h="100%"
+        templateRows="repeat(1, 1fr)"
+        templateColumns="200px 200px 1fr"
       >
-        <GridItem rowSpan={1} colSpan={1} bg="tomato">
-          {checkArrayDistance(data).map((x, i) => (
+        <GridItem
+          rowSpan={1}
+          colSpan={1}
+          p={3}
+          borderRight={'1px solid'}
+          borderRightColor={'gray.200'}
+        >
+          <Formik
+            initialValues={{
+              name: '',
+              lat: '',
+              lng: '',
+            }}
+            onSubmit={(values, actions) => {
+              locationListDispatch({ type: 'addLocationItem', value: values });
+              actions.setSubmitting(false);
+            }}
+          >
+            {props => (
+              <Form>
+                <Stack>
+                  <Field name="name">
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.name && form.touched.name}
+                      >
+                        <Input {...field} placeholder="場所の名前" size="sm" />
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="lat" validate={validateLatitude}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.lat && form.touched.lat}
+                      >
+                        <Input {...field} placeholder="経度" size="sm" />
+                        <FormErrorMessage>
+                          <Text fontSize={'xs'}>{form.errors.lat}</Text>
+                        </FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="lng" validate={validateLongitude}>
+                    {({ field, form }) => (
+                      <FormControl
+                        isInvalid={form.errors.lng && form.touched.lng}
+                      >
+                        <Input {...field} placeholder="経度" size="sm" />
+                        <FormErrorMessage>
+                          <Text fontSize={'xs'}>{form.errors.lng}</Text>
+                        </FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Button
+                    mt={4}
+                    size={'sm'}
+                    colorScheme="teal"
+                    leftIcon={<FaPlus />}
+                    isLoading={props.isSubmitting}
+                    type="submit"
+                  >
+                    追加する
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+        </GridItem>
+        <GridItem rowSpan={1} colSpan={1}>
+          <Accordion allowToggle>
+            {locationList.map((x, i) => (
+              <AccordionItem key={x.name}>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">
+                    <Box>{x.name}</Box>
+                    <Box fontSize="xs">
+                      {x.lat},{x.lng}
+                    </Box>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel pb={4}>
+                  {checkArrayDistance(locationList)[i].map((_x, _i) => (
+                    <Wrap mt={1} key={_x.diff}>
+                      <WrapItem>
+                        <Box>
+                          <Text fontSize={'sm'} fontWeight={'bold'}>
+                            {_x.distance}km
+                          </Text>
+                          <Text fontSize={'xs'}>{_x.diff}</Text>
+                        </Box>
+                      </WrapItem>
+                    </Wrap>
+                  ))}
+                </AccordionPanel>
+              </AccordionItem>
+            ))}
+          </Accordion>
+          {/* {checkArrayDistance(data).map((x, i) => (
             <Wrap border="1px" borderColor="gray.600" mt={5} p={2} key={i}>
               {x.map((_x, _i) => (
                 <WrapItem key={_i}>
@@ -103,9 +252,9 @@ export default function App() {
                 </WrapItem>
               ))}
             </Wrap>
-          ))}
+          ))} */}
         </GridItem>
-        <GridItem rowSpan={1} colSpan={4}>
+        <GridItem rowSpan={1} colSpan={1}>
           <MapContainer
             center={[34.6873377, 135.5237668]}
             zoom={12}
@@ -116,9 +265,9 @@ export default function App() {
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
 
-            {data.map((d, i) => (
+            {/* {data.map((d, i) => (
               <Circle center={[d.lat, d.lng]} radius={1000} key={i} position={[d.lat, d.lng]}/>
-            ))}
+            ))} */}
           </MapContainer>
         </GridItem>
       </Grid>
